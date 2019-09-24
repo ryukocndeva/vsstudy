8593151f690cfbcdecb8f2026f48b39a829cd315
ryukocndeva <ryukocndeva@gmail.com>
2019-09-24 06:59:40 -0700

    201909242259

---
+++ MSMQTest/ReceiveConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Messaging;
+
+namespace ReceiveConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiveMessageFromQueue(@".\Private$\TestQueue");
+        }
+
+        private static void ReceiveMessageFromQueue(string queueName)
+        {
+            MessageQueue msMq = new MessageQueue(queueName);
+
+            try
+            {
+                msMq.Formatter = new XmlMessageFormatter(new Type[] { typeof(Person)});
+                var message = (Person)msMq.Receive().Body;
+                Console.WriteLine("FirstName: " + message.FirstName + ", LastName: " + message.LastName);
+
+            }
+            catch (MessageQueueException ee)
+            {
+                Console.Write(ee.ToString());
+            }
+            catch (Exception eee)
+            {
+                Console.Write(eee.ToString());
+            }
+            finally
+            {
+                msMq.Close();
+            }
+            Console.WriteLine("Message received...");
+
+        }
+    }
+
+
+    public class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+    }
+}
---
+++ MSMQTest/SendConsoleApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Messaging;
+
+namespace SendConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessageToQueue(@".\Private$\TestQueue");
+        }
+
+
+        private static void SendMessageToQueue(string queueName)
+        {
+            MessageQueue msMq = null;
+            if (!MessageQueue.Exists(queueName))
+            {
+                msMq = MessageQueue.Create(queueName);
+            }
+            else
+            {
+                msMq = new MessageQueue(queueName);
+            }
+
+            int i = 0;
+
+            try
+            {
+                for (i = 0; i < 100; i++)
+                {
+                    Person p = new Person()
+                    {
+                        FirstName = "John",
+                        LastName = "Doe " + (++i).ToString()
+                    };
+                    msMq.Send(p);
+                }
+            }
+            catch (MessageQueueException ee)
+            {
+                Console.Write(ee.ToString());
+            }
+            catch (Exception eee)
+            {
+                Console.Write(eee.ToString());
+            }
+            finally
+            {
+                msMq.Close();
+            }
+            Console.WriteLine("Message sent...");
+        }
+
+    }
+
+    public class Person
+    {
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+    }
+}
